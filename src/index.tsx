7633b639f21a3117fbfb6e3fe1b45ca557338caa
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-12-29 05:53:31 -0300

    support folder

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import '@tremor/react/dist/esm/tremor.css'
-import './style.css';
+import './support/style.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
